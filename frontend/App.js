17c0119cac94a6d12faccbab4ec8b09b012715f3
Taylor-Stacey <taylor88stacey@gmail.com>
2022-06-08 12:08:41 -0700

    finished project

--- frontend/App.js
+++ frontend/App.js
@@ -4,9 +4,11 @@ import axios from "axios";
 import Charts from "./components/Charts";
 import Navbar from "./components/Navbar";
 
+import useDarkMode from "./hooks/useDarkMode";
+
 const App = () => {
   const [coinData, setCoinData] = useState([]);
-  const [darkMode, setDarkMode] = useState(false);
+  const [darkMode, setDarkMode] = useDarkMode(false);
 
   useEffect(() => {
     axios
@@ -16,6 +18,7 @@ const App = () => {
       .then(res => setCoinData(res.data))
       .catch(err => console.log(err));
   }, []);
+
   return (
     <div className={darkMode ? "dark-mode App" : "App"}>
       <Navbar darkMode={darkMode} setDarkMode={setDarkMode} />
